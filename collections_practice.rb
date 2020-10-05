45b99cf5e9a6ce8f833eb6e3b75ba251cbec79f5
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-10-05 01:09:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,9 +10,8 @@ def sort_array_char_count(array)
   array.sort_by{|a| a.length}
 end
 
-def swap_elements(array, x, y)
-  array[x], array[y] = array[y], array[x]
-  new_array
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
 end
 
 def reverse_array(array)
