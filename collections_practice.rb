7f9f78ba3aaa6046237f6bd00e0447974502c7c8
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-10-05 02:43:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,6 +32,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index|}
+  array.each_with_index.collect{|element, index| index != 1 ? element << "s"}
+  array 
   end
   
